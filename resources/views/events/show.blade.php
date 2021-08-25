2d97b572addaf16456a823e42a5aa94a4845f4e0
TevinMusau <tevinmutua@gmail.com>
2021-08-25 17:38:31 +0300

    Fixed some bugs and updated the UI

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -1,34 +1,38 @@
 @extends('layouts.app')
 @section('content')
     <div class="container-fluid">
-        <h2 class="text-center p-4">{{$event->user->username}}'s Event</h2>
+        <h2 class="text-center p-4 font-weight-bold">{{$event->user->username}}'s Event</h2>
         
         <div class="card mx-4">
             <div class="card-header">
                 <div class="text-center p-2">
-                    <span class="h5 p-2">About "{{$event->event_title}}" Event</span>
+                    <span class="h5 p-2 font-weight-bold">About "{{$event->event_title}}" Event</span>
                 </div>
             </div>
             <div class="card-body">
                 <div class="text-center">
-                    <p class="lead"><?php echo $event->about;?></p>
+                    <p class="lead" style="font-size: 1.02rem"><?php echo $event->about;?></p>
                 </div>
             </div>
             <div class="card-footer">
-                <span>Event Date:</span>
-                <span>{{$event->deadline}}</span> <br>
-                
-                <span>Event Location:</span>
-                <span>{{$event->location}}</span> <br>
-
-                <span>Event Creator:</span>
-                <span>{{$event->user->username}}</span> <br>
+                <span>
+                    <div id="first" class="text-right" style="float: left; margin: 0; padding: 0;">
+                        <p class="font-weight-bold font-italic p-3">Event Date:</p> 
+                        <p class="font-weight-bold font-italic p-3">Event Location:</p>
+                        <p class="font-weight-bold font-italic p-3">Event Creator:</p>
+                    </div>
+                    <div class="text-left" style="margin: 0; padding: 0">
+                        <p class="font-weight-bold font-italic p-3">{{$event->deadline}}</p>                
+                        <p class="font-weight-bold font-italic p-3">{{$event->location}}</p>                 
+                        <p class="font-weight-bold font-italic p-3">{{$event->user->username}}</p> 
+                    </div>
+                </span>
 
                 <div class="text-center">
                     {!!Form::open(['action'=>['EventUserController@store'],'method'=>'POST', 'class'=>'','style'=>'display: inline'])!!}
                             {{Form::hidden('user_id',Auth::user()->id)}}
                             {{Form::hidden('event_id',$event->id)}}
-                            {{Form::submit('Join Event',['class'=>'btn btn-outline-success', 'style'=>'width:15%; padding: 0.4rem'])}}
+                            {{Form::submit('Join Event',['class'=>'btn btn-outline-success font-weight-bold', 'style'=>'width:20%; padding: 0.4rem; box-shadow:  2px 2px 2px #B0E0E6'])}}
                     {!!Form::close()!!}
                 </div>
 
@@ -74,27 +78,27 @@
                     <a href="/pdf/event/{{$event->id}}" class="btn btn-link">Make PDF</a>
             </div> --}}
         {{-- </div> --}}
-        <h4 class="text-center p-4"> Joined this Event </h4>
+        <h3 class="text-center pt-5 pb-4 font-weight-bold"> Joined this Event </h3>
         
             @if (count($event->users)>0)
             <div class="row justify-content-center">
             <table class="mx-auto w-auto table table-responsive text-center">
                 <thead>
                     <tr>
-                        <th class="p-3">Name:</th>
-                        <th class="p-3">Username:</th>
-                        <th class="p-3">Email:</th>
-                        <th class="p-3">Admission Number:</th>
+                        <th class="p-3" style="font-size: 1.1rem">Name:</th>
+                        <th class="p-3" style="font-size: 1.1rem">Username:</th>
+                        <th class="p-3" style="font-size: 1.1rem">Email:</th>
+                        <th class="p-3" style="font-size: 1.1rem">Admission Number:</th>
                     </tr>
                 </thead>
                 
                 <tbody>
                     @foreach ($event->users as $item)
                     <tr>
-                        <td class="p-3">{{$item->name}}</td>
-                        <td class="p-3">{{$item->username}}</td>
-                        <td class="p-3">{{$item->email}}</td>
-                        <td class="p-3">{{$item->adm_no}}</td>
+                        <td class="p-3" style="font-size: 1.03rem">{{$item->name}}</td>
+                        <td class="p-3" style="font-size: 1.03rem">{{$item->username}}</td>
+                        <td class="p-3" style="font-size: 1.03rem">{{$item->email}}</td>
+                        <td class="p-3" style="font-size: 1.03rem">{{$item->adm_no}}</td>
                     </tr>
                     @endforeach
                 </tbody>
